c4a7d3d19cc8aa196169b86eb8f6ebc2ce34f862
Jhonatan C Hurtado <jhonatanchurtado@gmail.com>
2024-10-23 09:56:20 -0500

    add work experience section

--- js/app.js
+++ js/app.js
@@ -9,15 +9,18 @@ const light = document.querySelector(".light");
 const position = document.querySelector("#setPosition");
 const desc = document.querySelector("#setDescription");
 const desProfile = document.querySelector("#about-me-profesional");
-const ulSkills = document.querySelector("#ulSkills");
 const titleSkill = document.querySelector("#titleSkill");
+const titleExp = document.querySelector("#titleExp");
+const ulSkills = document.querySelector("#ulSkills");
+const ulExp = document.querySelector("#ulExp");
+const boxExp = document.querySelector("#boxExp");
 const body = document.querySelector("body");
-const host = window.location.host;
 const protocol = window.location.protocol;
 let skillList = [];
+let experienceList = [];
 let result = {};
 
-const getHost = window.location.host;
+const host = window.location.host;
 const pathName = window.location.pathname;
 
 const url =
@@ -38,6 +41,7 @@ loadInfo();
 const changeLang = (lang) => {
   const getLangValue = lang.target.value;
   ulSkills.innerHTML = "";
+  boxExp.innerHTML = "";
   getJobInfo(getLangValue);
 };
 LangOpt.addEventListener("change", changeLang);
@@ -48,7 +52,9 @@ const getJobInfo = async (lang = "es") => {
   position.textContent = result[lang].position;
   desc.innerHTML = result[lang].description;
   titleSkill.textContent = result[lang].titleSkill;
+  titleExp.textContent = result[lang].titleExp;
   getSkills(result[lang].skills);
+  getExperience(result[lang].workExp);
 };
 
 // Cambiar tema - claro / oscuro.
@@ -77,5 +83,41 @@ const getSkills = (skills) => {
     img.src = skill.icon;
     img.className = "icon-logo";
     item.prepend(img);
+  });
+};
+
+const getExperience = (experiences) => {
+  experiences.map((exp) => {
+    // console.log(exp);
+    const boxInfo = document.createElement("div");
+    boxInfo.id = "boxInfo";
+
+    const hTitle = document.createElement("h4");
+    hTitle.textContent = exp.company;
+
+    const positionJob = document.createElement("p");
+    positionJob.className = "positionJob";
+    positionJob.textContent = `Cargo: ${exp.position}`;
+
+    const timeJobBox = document.createElement("div");
+    timeJobBox.id = "timeJob";
+
+    const divideHr = document.createElement("hr");
+    divideHr.className = "divideHr";
+
+    const timeJobDate = document.createElement("small");
+    timeJobDate.textContent = `${exp.start_date} - ${
+      !exp.end_date ? "Actualmente" : exp.end_date
+    }`;
+
+    const descJob = document.createElement("p");
+    descJob.textContent = exp.descExp;
+
+    boxExp.append(boxInfo);
+    boxInfo.append(hTitle);
+    boxInfo.append(positionJob);
+    boxInfo.append(timeJobDate);
+    boxInfo.append(divideHr);
+    boxInfo.append(descJob);
   });
 };
