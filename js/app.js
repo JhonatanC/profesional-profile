d19e11b7269238cef534918d0921a9775adae57b
Jhonatan C Hurtado <jhonatanchurtado@gmail.com>
2025-01-29 13:26:17 -0500

    fix functions for perfomance

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ const pathName = window.location.pathname;
 const body = document.querySelector("body");
 const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
-//const fileDownBlack = document.querySelector(".file-down-black");
-//const fileDownWhite = document.querySelector(".file-down-white");
 const ulExp = document.querySelector("#ulExp");
 const theme = document.querySelector("#theme");
 const boxExp = document.querySelector("#boxExp");
@@ -23,9 +21,11 @@ const ulSkills = document.querySelector("#ulSkills");
 const desc = document.querySelector("#setDescription");
 const position = document.querySelector("#setPosition");
 const titleSkill = document.querySelector("#titleSkill");
-// const btnDownload = document.querySelector("#btnDownload");
 const desProfile = document.querySelector("#about-me-personal");
 const changeSettings = document.querySelector("#change-settings");
+//const fileDownBlack = document.querySelector(".file-down-black");
+//const fileDownWhite = document.querySelector(".file-down-white");
+// const btnDownload = document.querySelector("#btnDownload");
 // const textDownload = document.querySelector("#textDownload");
 // textDownload.textContent = lang == "es" ? `Descargar` : `Download`;
 
@@ -63,9 +63,6 @@ const getJobInfo = async (lang = "es") => {
   titleExp.textContent = result[lang].titleExp;
   getSkills(result[lang].skills);
   getExperience(lang, result[lang].workExp);
-  result[lang].workExp.map((work) => {
-    getTools(work.toolsApply);
-  });
 };
 
 // Cambiar tema - claro / oscuro.
@@ -94,13 +91,6 @@ const createDynamicElement = (setElement) => {
   `${!setElement.class ? null : (element.className = setElement.class)}`;
   `${!setElement.text ? null : (element.textContent = setElement.text)}`;
   `${!setElement.source ? null : (element.src = setElement.source)}`;
-
-  if (!setElement.position) {
-    setElement?.render.append(element);
-  } else {
-    setElement?.render?.prepend(element);
-  }
-
   return element;
 };
 
@@ -109,105 +99,87 @@ const getSkills = (skills) => {
   boxExp.innerHTML = "";
 
   skills.map((skill) => {
-    // console.log(skill);
     const liSkill = createDynamicElement({
       ele: "li",
       text: skill.language,
-      render: ulSkills,
-      position: "append",
     });
-
-    createDynamicElement({
+    const imgSkills = createDynamicElement({
       ele: "img",
       source: skill.icon,
       class: "icon-logo",
-      render: liSkill,
-      position: "preppend",
     });
-  });
-};
-
-const getTools = (tools) => {
-  tools.map((tool) => {
-    createDynamicElement({
-      ele: "li",
-      text: tool,
-      class: "tools-items",
-      render: boxInfo,
-    });
+    ulSkills.append(liSkill);
+    liSkill.prepend(imgSkills);
   });
 };
 
 const getExperience = (lang, experiences) => {
   experiences.map((exp) => {
     // Caja contenedora principal de información.
-    const boxInfo = createDynamicElement({
-      ele: "div",
-      id: "boxInfo",
-      render: boxExp,
-    });
+    const boxInfo = createDynamicElement({ ele: "div", id: "boxInfo" });
+    boxExp.append(boxInfo);
 
     // Nombre de la empresa.
-    createDynamicElement({ ele: "h4", text: exp.company, render: boxInfo });
+    const titleCompany = createDynamicElement({ ele: "h4", text: exp.company });
+    boxInfo.append(titleCompany);
 
     // Cargo en la empresa.
-    createDynamicElement({
+    const positionCompany = createDynamicElement({
       ele: "p",
+      class: "positionJob",
       text:
         lang == "es" ? `Cargo: ${exp.position}` : `Position: ${exp.position}`,
-      class: "positionJob",
-      render: boxInfo,
     });
+    boxInfo.append(positionCompany);
 
     // Contenedor tiempo en la empresa.
-    createDynamicElement({ ele: "div", id: "timeJob", render: boxInfo });
+    const boxTimeExp = createDynamicElement({ ele: "div", id: "timeJob" });
+    boxInfo.append(boxTimeExp);
 
     // Rango de tiempo en la empresa.
-    createDynamicElement({
+    const timeJobExp = createDynamicElement({
       ele: "small",
       text: `${exp.start_date} - ${
         !exp.end_date ? "Actualmente" : exp.end_date
       }`,
-      render: boxInfo,
     });
+    boxInfo.append(timeJobExp);
 
     // Separador
-    createDynamicElement({ ele: "hr", class: "divideHr", render: boxInfo });
+    const lineBreak = createDynamicElement({ ele: "hr", class: "divideHr" });
+    boxInfo.append(lineBreak);
 
     // Descripción actividades en la empresa.
-    createDynamicElement({ ele: "p", text: exp.descExp, render: boxInfo });
+    const descriptionActivities = createDynamicElement({
+      ele: "p",
+      text: exp.descExp,
+    });
+    boxInfo.append(descriptionActivities);
 
     // Contenedor - tecnologías utlizadas.
-    const ulTools = createDynamicElement({
-      ele: "ul",
-      id: "listTools",
-      render: boxInfo,
-    });
+    const ulTools = createDynamicElement({ ele: "ul", id: "listTools" });
+    boxInfo.append(ulTools);
 
-    // toolsApply
-    const lis = exp.toolsApply.map((tool) => {
-      createDynamicElement({
+    // Tecnologías implementadas
+    const technologies = exp.toolsApply.map((tool) => {
+      const liTools = createDynamicElement({
         ele: "li",
-        text: tool,
         class: "tools-items",
-        render: ulTools,
-        position: "append",
+        text: tool,
       });
+      ulTools.append(liTools);
     });
-
-    // console.log(lis);
 
     // Texto tecnologías
-    createDynamicElement({
+    const technologiesPrefix = createDynamicElement({
       ele: "span",
       text:
         lang == "es"
           ? `Tecnologías implementadas: `
           : `Technologies implemented: `,
-      render: ulTools,
       class: "pre-text-tec",
-      position: "prepend",
     });
+    ulTools.prepend(technologiesPrefix);
   });
 };
 
